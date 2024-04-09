9a4bf156b58d444bd059228ba74b97e534735dc7
ilman01 <ilmanzidni2006@gmail.com>
2024-04-09 14:10:20 +0700

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="unbirthday",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         #no packages needed
